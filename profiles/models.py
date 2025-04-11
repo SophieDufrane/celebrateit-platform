4a391ce7a0a49b5c82e59b559372b6eb38e5db7c
Sophie <dufranesophie@gmail.com>
2025-04-11 13:17:04 +0100

    Rename Profile model to UserProfile and add signal to auto-create on user creation

--- profiles/models.py
+++ profiles/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
 
-class Profile(models.Model):
+class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -18,4 +19,11 @@ class Profile(models.Model):
     def __str__(self):
         return f"{self.user}'s profile"
 
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+post_save.connect(create_user_profile, sender=User)
 
