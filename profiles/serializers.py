8ebe44f29ec68bf3bb8ebfa3482be9d708cebb8d
Sophie <dufranesophie@gmail.com>
2025-04-12 19:14:33 +0100

    Add custom permission and is_user_profile method field to UserProfile

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -6,12 +6,17 @@ class UserProfileSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
     first_name = serializers.ReadOnlyField(source='user.first_name')
     last_name = serializers.ReadOnlyField(source='user.last_name')
+    is_user_profile = serializers.SerializerMethodField()
 
+    def get_is_user_profile(self, obj):
+        request = self.context['request']
+        return request.user == obj.user
 
     class Meta:
         model = UserProfile
         fields = [
             'id', 'user', 'first_name', 'last_name', 'created_at', 
-            'updated_at', 'presentation', 'department', 'image'
+            'updated_at', 'presentation', 'department', 'image', 
+            'is_user_profile',
         ]
 
--- profiles/views.py
+++ profiles/views.py
@@ -1,34 +1,45 @@
+from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import UserProfile
 from .serializers import UserProfileSerializer
-from django.http import Http404
-from rest_framework import status
+from celebrateit_api.permissions import IsUserProfileOrReadOnly
 
 
 class UserProfileList(APIView):
     def get(self, request):
         user_profiles = UserProfile.objects.all()
-        serializer = UserProfileSerializer(user_profiles, many=True)
+        serializer = UserProfileSerializer(
+            user_profiles, many=True, context={'request': request}
+        )
         return Response(serializer.data)
 
 
 class UserProfileDetail(APIView):
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsUserProfileOrReadOnly]
+
     def get_object(self, pk):
         try:
             user_profile = UserProfile.objects.get(pk=pk)
+            self.check_object_permissions(self.request, user_profile)
             return user_profile
         except UserProfile.DoesNotExist:
             raise Http404
     
     def get(self, request, pk):
         user_profile = self.get_object(pk)
-        serializer = UserProfileSerializer(user_profile)
+        serializer = UserProfileSerializer(
+            user_profile, context={'request': request}
+        )
         return Response(serializer.data)
     
     def put(self, request, pk):
         user_profile = self.get_object(pk)
-        serializer = UserProfileSerializer(user_profile, data=request.data)
+        serializer = UserProfileSerializer(
+            user_profile, data=request.data, context={'request': request}
+        )
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
