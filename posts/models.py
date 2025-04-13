d692bae8b1ce628f8976670e0f252c165b560c9b
Sophie <dufranesophie@gmail.com>
2025-04-13 18:26:02 +0100

    Add PostDetail view with permission

--- posts/models.py
+++ posts/models.py
@@ -3,7 +3,11 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=255)
--- posts/views.py
+++ posts/views.py
@@ -1,15 +1,24 @@
 from rest_framework import generics
 from .models import Post
 from .serializers import PostSerializer
+from celebrateit_api.permissions import IsUserProfileOrReadOnly
 
 
 class PostList(generics.ListCreateAPIView):
     """
     List all posts or create a new one.
-    
     """
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, update, or delete a post if the user is the owner of the post.
+    """
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsUserProfileOrReadOnly]
