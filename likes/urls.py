9acd294d28a8e706d74391f075ed8fb523060f94
Sophie <dufranesophie@gmail.com>
2025-04-18 18:41:38 +0100

    Add docstring to likes URL patterns (flake8 clean)

--- likes/urls.py
+++ likes/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from likes import views
 
+"""
+URL patterns for the Like app.
+Includes list, create, retrieve, and delete views.
+"""
+
 urlpatterns = [
     path('likes/', views.LikeList.as_view()),
     path('likes/<int:pk>/', views.LikeDetail.as_view()),
