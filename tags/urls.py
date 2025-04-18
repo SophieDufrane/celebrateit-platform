788c2847377b7708ff59d3cfab8f07c0db1b5fda
Sophie <dufranesophie@gmail.com>
2025-04-18 18:48:31 +0100

    Add docstring to tag URL patterns (flake8 clean)

--- tags/urls.py
+++ tags/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from tags import views
 
+"""
+URL patterns for the Tag app.
+Includes list view for all tags.
+"""
+
 urlpatterns = [
     path('tags/', views.TagList.as_view()),
 ]
