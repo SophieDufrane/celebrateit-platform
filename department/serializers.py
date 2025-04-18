1a528200258c68c668d1f8582c4d64233e61e861
Sophie <dufranesophie@gmail.com>
2025-04-18 18:53:06 +0100

    Add docstring to DepartmentSerializer (flake8 clean)

--- department/serializers.py
+++ department/serializers.py
@@ -3,7 +3,10 @@ from .models import Department
 
 
 class DepartmentSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Department model.
+    Includes code, name, and optional icon used to group users by department.
+    """
     class Meta:
         model = Department
         fields = ['id', 'code', 'name', 'icon']
-
