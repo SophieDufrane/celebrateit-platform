3ca51278457bc6bee97ce646bd90db07670e5694
Sophie <dufranesophie@gmail.com>
2025-04-18 18:44:56 +0100

    Add docstring to TagSerializer (flake8 clean)

--- tags/serializers.py
+++ tags/serializers.py
@@ -3,7 +3,10 @@ from .models import Tag
 
 
 class TagSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Tag model.
+    Used for displaying tag name and slug in tag listings and filters.
+    """
     class Meta:
         model = Tag
         fields = ['id', 'name', 'slug']
-
