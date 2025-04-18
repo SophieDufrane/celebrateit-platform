8ed40ab8c8dc8c0389cb0412e5651021b75ecb88
Sophie <dufranesophie@gmail.com>
2025-04-18 18:43:45 +0100

    Add docstring and alphabetical ordering to Tag model (flake8 clean)

--- tags/models.py
+++ tags/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 from django.utils.text import slugify
 
+
 class Tag(models.Model):
+    """
+    Tag model used for categorizing recognition stories and nominations.
+    Each tag has a name and a unique slug.
+    """
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=60, unique=True, blank=True)
 
@@ -12,3 +17,6 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        ordering = ['name']
