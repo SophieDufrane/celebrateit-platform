5b4739d833fcef0980ee0c9584233d7cd64b9fed
Sophie <dufranesophie@gmail.com>
2025-04-18 18:54:18 +0100

    Add department list view with docstring (flake8 clean)

--- department/views.py
+++ department/views.py
@@ -2,11 +2,11 @@ from rest_framework import generics
 from .models import Department
 from .serializers import DepartmentSerializer
 
+
 class DepartmentList(generics.ListAPIView):
     """
     List all departments.
-    
+    Used for filtering users or categorizing profiles by department.
     """
     queryset = Department.objects.all()
     serializer_class = DepartmentSerializer
-
