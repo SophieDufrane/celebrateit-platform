fedde61faab8c429d197d6fc5b9d1d520d5faef5
Sophie <dufranesophie@gmail.com>
2025-04-18 18:47:18 +0100

    Add docstring to TagList view (flake8 clean)

--- tags/views.py
+++ tags/views.py
@@ -2,10 +2,11 @@ from rest_framework import generics
 from .models import Tag
 from .serializers import TagSerializer
 
+
 class TagList(generics.ListAPIView):
     """
     List all available tags.
-    
+    Useful for filtering or categorizing posts and nominations.
     """
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
