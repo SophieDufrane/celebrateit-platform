605abb26ead616bd9cc7e9e3a4405d61082d87f9
Sophie <dufranesophie@gmail.com>
2025-04-18 18:02:38 +0100

    Add docstring and named URL patterns to nominations URLs

--- nominations/urls.py
+++ nominations/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from nominations import views
 
+"""
+URL patterns for the Nomination app.
+Includes list, create, retrieve, update, and delete views.
+"""
+
 urlpatterns = [
     path('nominations/', views.NominationList.as_view()),
     path('nominations/<int:pk>/', views.NominationDetail.as_view()),
