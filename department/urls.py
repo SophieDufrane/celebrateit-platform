016447e2b42397636dcb404dbf1222b0bd86264d
Sophie <dufranesophie@gmail.com>
2025-04-18 18:56:20 +0100

    Add docstring to department URL patterns (flake8 clean)

--- department/urls.py
+++ department/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from department import views
 
+"""
+URL patterns for the Department app.
+Includes a list view for all departments.
+"""
+
 urlpatterns = [
     path('department/', views.DepartmentList.as_view()),
 ]
