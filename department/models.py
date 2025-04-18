ad4259388bf62dfb0643a7d0b7916ccaf5b7b3e8
Sophie <dufranesophie@gmail.com>
2025-04-18 18:51:50 +0100

    Add docstring and alphabetical ordering to Department model (flake8 clean)

--- department/models.py
+++ department/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 
+
 class Department(models.Model):
+    """
+    Department model used to group users by team.
+    Includes a name, short code, and optional icon.
+    """
     name = models.CharField(max_length=50, blank=True)
     code = models.CharField(max_length=10, unique=True)
     icon = models.ImageField(
         upload_to='department_icons/',
         blank=True, null=True
     )
+
+    class Meta:
+        ordering = ['name']
 
     def __str__(self):
         return self.name
