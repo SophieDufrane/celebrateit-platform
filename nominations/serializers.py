8be6dbe9f9559518bb862d66331475033f4cf6c3
Sophie <dufranesophie@gmail.com>
2025-04-21 16:51:26 +0100

    Refactor Nomination: update tag field to display tag name and its ID

--- nominations/serializers.py
+++ nominations/serializers.py
@@ -10,21 +10,22 @@ class NominationSerializer(serializers.ModelSerializer):
     Links the nomination to a nominee and a single tag.
     Includes frontend user ownership check.
     """
+    is_user = serializers.SerializerMethodField()
     nominator = serializers.ReadOnlyField(source='nominator.username')
     nominee = serializers.PrimaryKeyRelatedField(
         queryset=User.objects.all()
     )
     nominee_username = serializers.ReadOnlyField(source='nominee.username')
-    is_user = serializers.SerializerMethodField()
     tag = serializers.PrimaryKeyRelatedField(
         queryset=Tag.objects.all()
     )
+    tag_name = serializers.ReadOnlyField(source='tag.name')
 
     class Meta:
         model = Nomination
         fields = [
-            'id', 'nominator', 'is_user', 'nominee', 'nominee_username', 
-            'title', 'content', 'created_at', 'updated_at', 'tag',
+            'id', 'is_user', 'nominator', 'nominee', 'nominee_username', 
+            'title', 'content', 'tag', 'tag_name', 'created_at', 'updated_at',
         ]
 
     def get_is_user(self, obj):
