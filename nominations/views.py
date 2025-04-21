bf144eaafd0b1c99f97e674d9cdc85370e50fcc2
Sophie <dufranesophie@gmail.com>
2025-04-21 15:54:44 +0100

    Refactor Nominations views: update search options and add filter by tags

--- nominations/views.py
+++ nominations/views.py
@@ -1,4 +1,6 @@
-from rest_framework import generics, permissions
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Nomination
 from .serializers import NominationSerializer
 from celebrateit_api.permissions import IsNominatorOrReadOnly
@@ -8,10 +10,23 @@ class NominationList(generics.ListCreateAPIView):
     """
     List all nominations or create a new one if authenticated.
     Each nomination includes a nominee, title, message, and a required tag.
+    Adds likes and comments count annotations.
+    Allows searching by nominator's name, and filtering by tag or department.
     """
-    queryset = Nomination.objects.all()
+    queryset = Nomination.objects.annotate(
+        likes_count=Count('likes', distinct=True),
+        comments_count=Count('comment', distinct=True)
+    ).order_by('-created_at')
     serializer_class = NominationSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend,]
+    filterset_fields = ['nominator__profile__department', 'tags', 'nominee']
+    search_fields = [
+    'nominator__first_name',
+    'nominator__last_name',
+    'nominee__first_name',
+    'nominee__last_name',
+    ]
 
     def perform_create(self, serializer):
         serializer.save(nominator=self.request.user)
