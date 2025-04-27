fb9ece01229a48f223a7b95746c873747f4b3b7c
Sophie <dufranesophie@gmail.com>
2025-04-27 15:11:44 +0100

    Install and configure React Router with initial routes for Home, Log In, Register, Page not found

--- celebrateit_frontend/src/components/NavBar.js
+++ celebrateit_frontend/src/components/NavBar.js
@@ -1,18 +1,27 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.Navbar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand href="#home">CelebrateIt</Navbar.Brand>
+        <Navbar.Brand as={NavLink} to="/">
+          CelebrateIt
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link>Home</Nav.Link>
-            <Nav.Link>Sign in</Nav.Link>
-            <Nav.Link>Sign up</Nav.Link>
+            <Nav.Link as={NavLink} exact to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/signin">
+              Sign in
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/signup">
+              Sign up
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
