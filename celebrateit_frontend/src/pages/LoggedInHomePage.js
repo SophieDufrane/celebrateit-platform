16ead9c6ba04ac8b0f5d0ade12288dc3c008fb54
Sophie <dufranesophie@gmail.com>
2025-04-27 16:49:42 +0100

    Create LoggedInHomePage component and update home route

---
+++ celebrateit_frontend/src/pages/LoggedInHomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LoggedInHomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to CelebrateIt Feed</h1>
+      {/* Placeholder for Recognitions and Nominations feed */}
+    </div>
+  );
+};
+
+export default LoggedInHomePage;
