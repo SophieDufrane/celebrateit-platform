5b537fc6e39e28ca7c85870065bf58c08b43b683
Sophie <dufranesophie@gmail.com>
2025-04-27 09:11:49 +0100

    Build and style Navbar using React-Bootstrap

---
+++ celebrateit_frontend/src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+
+const NavBar = () => {
+  return (
+    <Navbar expand="md" fixed="top">
+      <Container>
+        <Navbar.Brand href="#home">CelebrateIt</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+            <Nav.Link>Home</Nav.Link>
+            <Nav.Link>Sign in</Nav.Link>
+            <Nav.Link>Sign up</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
