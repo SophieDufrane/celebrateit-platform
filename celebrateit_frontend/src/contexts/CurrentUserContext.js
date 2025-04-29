374a160b18e4289c955d6c6e168425a845727a01
Sophie <dufranesophie@gmail.com>
2025-04-29 17:50:38 +0100

    Create CurrentUserContext to store user data across app and refactor index.js to wrap App with provider

---
+++ celebrateit_frontend/src/contexts/CurrentUserContext.js
@@ -0,0 +1,19 @@
+import { createContext, useContext, useState } from "react";
+
+export const CurrentUserContext = createContext();
+export const SetCurrentUserContext = createContext();
+
+export const useCurrentUser = () => useContext(CurrentUserContext);
+export const useSetCurrentUser = () => useContext(SetCurrentUserContext);
+
+export const CurrentUserProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  return (
+    <CurrentUserContext.Provider value={currentUser}>
+      <SetCurrentUserContext.Provider value={setCurrentUser}>
+        {children}
+      </SetCurrentUserContext.Provider>
+    </CurrentUserContext.Provider>
+  );
+};
