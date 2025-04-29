1ffdd920ae3c20871b63e22694117d0a8821316e
Sophie <dufranesophie@gmail.com>
2025-04-29 15:06:15 +0100

    Set up minimal SignInForm structure using Bootstrap Form and React Router Link

--- celebrateit_frontend/src/pages/auth/SignInForm.js
+++ celebrateit_frontend/src/pages/auth/SignInForm.js
@@ -1,11 +1,48 @@
 import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import { Link } from "react-router-dom";
 
-const SignInForm = () => {
+function SignInForm() {
   return (
-    <div>
-      <h1>Sign In</h1>
-    </div>
+    <Row className="my-4">
+      <Col className="p-4">
+        <Container>
+          <h1>Please sign in</h1>
+          <Form>
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Username"
+                name="username"
+              />
+            </Form.Group>
+
+            <Form.Group controlId="password">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password"
+              />
+            </Form.Group>
+
+            <Button type="submit">Sign In</Button>
+          </Form>
+        </Container>
+
+        <Container>
+          <Link to="/register">
+            Don't have an account? <span>Register now!</span>
+          </Link>
+        </Container>
+      </Col>
+    </Row>
   );
-};
+}
 
 export default SignInForm;
