7468972f4aefb1142bf251847620e4e98cf3d499
Sophie <dufranesophie@gmail.com>
2025-04-29 15:24:51 +0100

    Create minimal SignInForm with working login request

--- celebrateit_frontend/src/pages/auth/SignInForm.js
+++ celebrateit_frontend/src/pages/auth/SignInForm.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
@@ -7,18 +8,44 @@ import Container from "react-bootstrap/Container";
 import { Link } from "react-router-dom";
 
 function SignInForm() {
+  const [signInData, setSignInData] = useState({
+    username: "",
+    password: "",
+  });
+  const { username, password } = signInData;
+
+  function handleChange(event) {
+    setSignInData({
+      ...signInData,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      const { data } = await axios.post("/dj-rest-auth/login/", signInData);
+      console.log("Login successful:", data);
+      alert("Login success!");
+    } catch (err) {
+      console.error("Login failed:", err.response?.data);
+    }
+  }
+
   return (
     <Row className="my-4">
       <Col className="p-4">
         <Container>
           <h1>Please sign in</h1>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group controlId="username">
               <Form.Label className="d-none">Username</Form.Label>
               <Form.Control
                 type="text"
                 placeholder="Username"
                 name="username"
+                value={username}
+                onChange={handleChange}
               />
             </Form.Group>
 
@@ -28,6 +55,8 @@ function SignInForm() {
                 type="password"
                 placeholder="Password"
                 name="password"
+                value={password}
+                onChange={handleChange}
               />
             </Form.Group>
 
