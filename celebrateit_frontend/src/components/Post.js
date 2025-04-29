eecf674c8dda5d0a61819ddde4f05ae3b9adbe60
Sophie <dufranesophie@gmail.com>
2025-04-29 11:53:42 +0100

    Refactor Post component: destructure props inside and add children support

--- celebrateit_frontend/src/components/Post.js
+++ celebrateit_frontend/src/components/Post.js
@@ -2,66 +2,71 @@ import React from "react";
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-const Post = ({
-  title,
-  content,
-  image,
-  display_name,
-  created_at,
-  likes_count,
-  comments_count,
-  id,
-}) => {
+const Post = (props) => {
+  const {
+    id,
+    title,
+    content,
+    image,
+    display_name,
+    created_at,
+    likes_count,
+    comments_count,
+    children,
+  } = props;
+
   return (
-    <Link
-      to={`/posts/${id}`}
-      style={{ textDecoration: "none", color: "inherit" }}
-    >
-      <Card className="mb-3">
-        <Card.Body>
-          <div className="d-flex align-items-center justify-content-between mb-3">
-            {/* Icon Placeholder */}
-            <div
-              style={{
-                width: "40px",
-                height: "40px",
-                borderRadius: "50%",
-                backgroundColor: "#ccc",
-              }}
-            ></div>
+    <Card className="mb-3">
+      <Card.Body>
+        <div className="d-flex align-items-center justify-content-between mb-3">
+          {/* Icon Placeholder */}
+          <div
+            style={{
+              width: "40px",
+              height: "40px",
+              borderRadius: "50%",
+              backgroundColor: "#ccc",
+            }}
+          ></div>
 
-            {/* Author name */}
-            <strong className="ml-2 flex-grow-1">{display_name}</strong>
-            {/* Date */}
-            <small className="text-muted">{created_at}</small>
-          </div>
+          {/* Author name */}
+          <strong className="ml-2 flex-grow-1">{display_name}</strong>
+          {/* Date */}
+          <small className="text-muted">{created_at}</small>
+        </div>
 
+        {/* Clickable area */}
+        <Link
+          to={`/posts/${id}`}
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
           {/* Title */}
           <Card.Title>{title}</Card.Title>
           {/* Content */}
           <Card.Text>{content}</Card.Text>
-        </Card.Body>
+          {/* Image */}
+          {image && <Card.Img variant="top" src={image} alt={title} />}
+        </Link>
+        {/* Action buttons (Delete/Edit etc.) */}
+        {children}
+      </Card.Body>
 
-        {/* Image */}
-        {image && <Card.Img variant="top" src={image} alt={title} />}
-
-        {/* Like and Comment icons */}
-        <div className="d-flex justify-content-around mt-3">
-          <div className="text-center">
-            <i className="far fa-heart fa-lg"></i>
-            <div>
-              <small>{likes_count}</small>
-            </div>
-          </div>
-          <div className="text-center">
-            <i className="far fa-comment fa-lg"></i>
-            <div>
-              <small>{comments_count}</small>
-            </div>
+      {/* Like and Comment icons */}
+      <div className="d-flex justify-content-around mt-3">
+        <div className="text-center">
+          <i className="far fa-heart fa-lg"></i>
+          <div>
+            <small>{likes_count}</small>
           </div>
         </div>
-      </Card>
-    </Link>
+        <div className="text-center">
+          <i className="far fa-comment fa-lg"></i>
+          <div>
+            <small>{comments_count}</small>
+          </div>
+        </div>
+      </div>
+    </Card>
   );
 };
 
