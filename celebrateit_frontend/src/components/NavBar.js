94416c74a4073576afe5c7b6902bcccbe12d76a9
Sophie <dufranesophie@gmail.com>
2025-05-02 10:08:28 +0100

    Update NavBar to reflect user login state and display user name

--- celebrateit_frontend/src/components/NavBar.js
+++ celebrateit_frontend/src/components/NavBar.js
@@ -2,13 +2,18 @@ import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
+import { useCurrentUser } from "../contexts/CurrentUserContext";
 
 const NavBar = () => {
+  const currentUser = useCurrentUser();
+  const displayName = currentUser?.first_name || currentUser?.username;
+
   return (
     <Navbar className={styles.Navbar} expand="md" fixed="top">
       <Container>
         <Navbar.Brand as={NavLink} to="/">
-          CelebrateIt
+          CelebrateIt{" "}
+          {currentUser && `| ${currentUser.first_name || currentUser.username}`}
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
@@ -16,21 +21,35 @@ const NavBar = () => {
             <Nav.Link as={NavLink} exact to="/">
               Home
             </Nav.Link>
-            <NavLink to="/posts/create" className="nav-link">
-              Recognize
-            </NavLink>
-            <NavLink to="/nominations/create" className="nav-link">
-              Nominate
-            </NavLink>
-            <NavLink to="/profile" className="nav-link">
-              Profile
-            </NavLink>
-            <Nav.Link as={NavLink} to="/login">
-              Log In
-            </Nav.Link>
-            <Nav.Link as={NavLink} to="/register">
-              Register
-            </Nav.Link>
+
+            {currentUser ? (
+              <>
+                <NavLink to="/posts/create" className="nav-link">
+                  Recognize
+                </NavLink>
+                <NavLink to="/nominations/create" className="nav-link">
+                  Nominate
+                </NavLink>
+                <NavLink
+                  to={`/profiles/${currentUser?.profile_id}`}
+                  className="nav-link"
+                >
+                  Profile
+                </NavLink>
+                <Nav.Link as={NavLink} to="/logout">
+                  Logout
+                </Nav.Link>
+              </>
+            ) : (
+              <>
+                <Nav.Link as={NavLink} to="/login">
+                  Log In
+                </Nav.Link>
+                <Nav.Link as={NavLink} to="/register">
+                  Register
+                </Nav.Link>
+              </>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
