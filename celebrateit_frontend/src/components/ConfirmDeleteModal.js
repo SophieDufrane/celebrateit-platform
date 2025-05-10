ba5ec8d2402d889df94b53afb988627747369081
Sophie <dufranesophie@gmail.com>
2025-05-10 15:24:18 +0100

    Create reusable ConfirmDeleteModal component

---
+++ celebrateit_frontend/src/components/ConfirmDeleteModal.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Modal, Button } from "react-bootstrap";
+
+function ConfirmDeleteModal({
+  show,
+  onHide,
+  onConfirm,
+  message = "Are you sure you want to delete this post?",
+}) {
+  return (
+    <Modal show={show} onHide={onHide} centered>
+      <Modal.Header closeButton>
+        <Modal.Title>Confirm Deletion</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>{message}</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={onHide}>
+          Cancel
+        </Button>
+        <Button variant="danger" onClick={onConfirm}>
+          Delete
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default ConfirmDeleteModal;
