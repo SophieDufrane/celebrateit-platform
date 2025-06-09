3feed454a86d983bce6028cda9173b96c1f9eeb3
Sophie <dufranesophie@gmail.com>
2025-06-09 16:13:58 +0100

    Review utils.js: Add comments and TODO for error handling in fetchMoreData

--- celebrateit_frontend/src/utils/utils.js
+++ celebrateit_frontend/src/utils/utils.js
@@ -1,6 +1,7 @@
 import jwtDecode from "jwt-decode";
 import { axiosReq } from "../api/axiosDefaults";
 
+// Fetch next page of paginated data and merge with existing resource state
 export const fetchMoreData = async (resource, setResource) => {
   try {
     const { data } = await axiosReq.get(resource.next);
@@ -15,17 +16,22 @@ export const fetchMoreData = async (resource, setResource) => {
         prevResource.results
       ),
     }));
-  } catch (err) {}
+  } catch (err) {
+    // TODO: add user feedback or logging on fetch error
+  }
 };
 
+// Store refresh token expiry timestamp in localStorage
 export const setTokenTimestamp = (data) => {
   const refreshTokenTimestamp = jwtDecode(data?.refresh_token).exp;
   localStorage.setItem("refreshTokenTimestamp", refreshTokenTimestamp);
 };
 
+// Check if refresh token timestamp exists to determine if token should be refreshed
 export const shouldRefreshToken = () =>
   !!localStorage.getItem("refreshTokenTimestamp");
 
+// Remove refresh token timestamp from localStorage on logout or token invalidation
 export const removeTokenTimestamp = () => {
   localStorage.removeItem("refreshTokenTimestamp");
 };
